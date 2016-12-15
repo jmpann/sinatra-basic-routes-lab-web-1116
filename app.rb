19b5adb8c128285107bc9716d27690e721d9d3de
Josh Pann <josh.pann@example.com>
2016-12-15 11:49:21 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Josh"
+  end
+
+  get '/hometown' do
+    "My hometown is Pleasanton"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hotline Bling by Drake"
+  end
+  
 end
